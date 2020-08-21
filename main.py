49a0bf205a3571ee098083bab511ff7e2d8a87b1
Derry Henderson <cldh0910@cl.glow.scot>
2020-08-21 09:20:45 +0000

    Finished project

--- main.py
+++ main.py
@@ -2,34 +2,43 @@
 
 PupilNames = []
 prices = []
+NoOfPupils = 0
 
 #get group name
 GroupName = input("Please enter group name:   ")
 
 #get a valid number of pupils in the group
-NoOfPupils = int(input("Please enter number of pupils:   "))
+NoOfPupils = float(input("Please enter number of pupils:   "))
 
-if NoOfPupils != int(NoOfPupils):
-  NoOfPupils = int(input("Please enter a valid number of pupils:   "))
+while not NoOfPupils.is_integer():
+  NoOfPupils = float(input("Please enter number of pupils:   "))
+
+NoOfPupils = int(NoOfPupils)
 
 
+#Loop for each pupil in the group
 for Counter in range (NoOfPupils) :
     #Get pupil Name
     PupilName = input("Please enter pupil name:   ") 
     PupilNames.append(PupilName)
 
+    #Ask if pupil wants to pre-order a photo
     Photo = input("would you like to pre order a photo?:   ").lower()
 
+    while Photo != "yes" and Photo != "no":
+      Photo = input("Please enter yes or no:   ")
+
+    #Decide and store ticket cost for each pupil
     price1 = 3.00
     price2 = 4.99
     if Photo == "yes":
-      ticketprice = str(PupilName) + "    £" + str(round(price2,2))
+      ticketprice = price2
     elif Photo == "no":
-      ticketprice = str(PupilName) + "    £" + str(round(price1,2))
+      ticketprice = price1
     prices.append(ticketprice)
     
-
+#Display results
 print ("Group Name:   " + str(GroupName))
 print ("Number in group:   " + str(NoOfPupils))
 for y in range(NoOfPupils):
-  print (prices[y])
+  print (f'{PupilNames[y]:20}{ticketprice:.2f}')
